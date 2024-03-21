622631c1711da4ac21792f841ec1264e4e7cc7a2
jmorris9750 <159032459+jmorris9750@users.noreply.github.com>
2024-03-21 09:10:03 -0400

    conversion rates

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
 
-// function showDrop1() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-//   }
-//   document.getElementById("usd") 
-
-//   document.getElementById("box1") 
-//   // Close the dropdown if the user clicks outside of it
-//   window.onclick = function(event) {
-//     if (!event.target.matches('.dropb')) {
-//       var dropdowns = document.getElementsByClassName("dropdown-content");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
-
 const in_amount = document.getElementById("amount");
 const dd_from = document.getElementById("from");
 const dd_to = document.getElementById("to");
 const div_result = document.getElementById("result");
 
+
 const exchangeRates = {
     usd: {
         pesos: 16.85,
-        usd:1
+        usd:1,
+        eur:0.92,
+        rupee:83.11,
+        yen:151.09
 
     },
     pesos: {
       usd: 0.059,
-      pesos: 1
+      pesos: 1,
+      yen:9.04,
+      rupee:4.97,
+      eur:0.055
+
+    },
+
+    yen: {
+      usd:0.0066,
+      pesos: 0.11,
+      yen:1,
+      rupee:0.55,
+      eur:0.0061
 
     }
 
-    
+
+
 
 
 }
 function convertCurrency() {
-
-  
-    var x = in_amount; 
-    if(dd_from == usd, dd_to == pesosusd) {
-      div_result.innerText = in_amount*;
+    if (dd_from.value = "usd", dd_to.value = "pesos") {
+      div_result.innerHTML = in_amount.value * exchangeRates.usd.pesos
     }
 
-}
+    else if (dd_from.value = "usd", dd_to.value = "yen") {
+      div_result.innerHTML = in_amount.value * exchangeRates.usd.yen
+    }
+
+    else if (dd_from.value = "usd", dd_to.value = "eur") {
+      div_result.innerHTML = in_amount.value * exchangeRates.usd.eur
+    }
+
+    else if (dd_from.value = "usd", dd_to.value = "inr") {
+      div_result.innerHTML = in_amount.value * exchangeRates.usd.rupee
+    }
+
+    else if (dd_from.value = "usd", dd_to.value = "usd") {
+      div_result.innerHTML = in_amount.value * exchangeRates.usd.usd
+    }
+
+    }
